a5da178e001940edc0665e5412138888b28ff8b6
Ignasi Fibla <ignasi.fibla@upc.edu>
2024-08-21 21:57:51 +0200

    Lap detection working

--- data-managment/spark-main.py
+++ data-managment/spark-main.py
@@ -1,4 +1,14 @@
-from pyspark.sql.types import ArrayType, MapType, FloatType, IntegerType, StringType
+from pyspark.sql.streaming.state import GroupStateTimeout, GroupState
+from pyspark.sql.types import (
+    ArrayType,
+    MapType,
+    FloatType,
+    IntegerType,
+    StringType,
+    StructType,
+    StructField,
+    DoubleType,
+)
 from pyspark.sql.functions import (
     col,
     collect_list,
@@ -10,18 +20,30 @@ from pyspark.sql.functions import (
     udf,
     when,
     lit,
+    size,
+    coalesce,
+    row_number,
 )
 from pyspark.sql.session import SparkSession
 from packets import Packet
 from track import Track
+from delta import *
+import pandas as pd
 
-## --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2
+pd.options.mode.chained_assignment = None
+
+## --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.2,io.delta:delta-spark_2.12:3.2.0
 
 spark = (
     SparkSession.builder.appName("DataDash")
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+    .config(
+        "spark.sql.catalog.spark_catalog",
+        "org.apache.spark.sql.delta.catalog.DeltaCatalog",
+    )
     .config("spark.sql.adaptive.enabled", True)
-    .getOrCreate()
 )
+spark = configure_spark_with_delta_pip(spark).getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
 ingest_decode_explode = (
@@ -82,10 +104,6 @@ ingest_decode_explode = (
             MapType(StringType(), FloatType()),
         )(col("driver_packet")),
     )
-    .writeStream.format("console")
-    .outputMode("update")
-    .start()
-    .awaitTermination()
 )
 
 speed_layer = (
@@ -108,4 +126,70 @@ speed_layer = (
     # .awaitTermination()
 )
 
-batch_layer = ingest_decode_explode.groupby("driver").agg(collect_list("driver_packet"))
+output_schema = StructType(
+    [
+        StructField("driver", IntegerType(), True),
+        StructField("session_time", FloatType(), True),
+        StructField("position", FloatType(), True),
+        StructField("lap", IntegerType(), True),
+    ]
+)
+
+
+def rewrite_df(generator, lap):
+    rows = pd.concat(generator, ignore_index=True)
+    rows.sort_values(by=["session_time"], inplace=True)
+
+    if not any(rows["lap"] != -1):
+        rows.iloc[0, rows.columns.get_loc("lap")] = lap
+
+    for i in range(1, len(rows)):
+        if rows.iloc[i].position == -1:
+            rows.iloc[i, rows.columns.get_loc("lap")] = lap
+            rows.iloc[i, rows.columns.get_loc("position")] = rows.iloc[i - 1].position
+        elif rows.iloc[i - 1].position > rows.iloc[i].position:
+            lap += 1
+            rows.iloc[i, rows.columns.get_loc("lap")] = lap
+        else:
+            rows.iloc[i, rows.columns.get_loc("lap")] = lap
+
+    return rows, lap
+
+
+def add_laps(key, pdf_iter, state):
+    current_lap = state.get[0] if state.exists else 1
+    pdf_iter, lap = rewrite_df(pdf_iter, current_lap)
+    state.update((lap,))
+    yield pdf_iter
+
+
+df_with_position = (
+    ingest_decode_explode.withColumn(
+        "position",
+        udf(
+            lambda x: x["world_position_perc"] if "world_position_perc" in x else None,
+            FloatType(),
+        )(col("driver_packet")),
+    )
+    .fillna(-1, subset=["position"])
+    .withColumn("lap", lit(-1))
+    .select(
+        col("driver"),
+        col("session_time"),
+        col("lap"),
+        col("position"),
+    )
+    .filter(col("driver") == 0)
+    .groupby("driver")
+    .applyInPandasWithState(
+        add_laps,
+        outputStructType=output_schema,
+        stateStructType="current_lap int",
+        outputMode="Update",
+        timeoutConf=GroupStateTimeout.NoTimeout,
+    )
+    .writeStream.format("console")
+    .outputMode("update")
+    .start()
+    .awaitTermination()
+)
